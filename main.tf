f0094bc3d615dda65a7e05c71859a5a405978f84
Minaz_Fatima <minazmohammed5@gmail.com>
2025-09-24 21:56:16 +0530

    Fix file path

--- main.tf
+++ main.tf
@@ -1,10 +1,9 @@
-resource "local_file" "my_pet" {
-  filename = "${path.module}/pets.txt"
-  content  = "I Love Pets"
-}
-
 resource "random_pet" "my_pet" {
   prefix    = "Miss"
   separator = "."
   length    = "1"
+}
+resource "local_file" "my_pet" {
+  filename = "${path.module}/pets.txt"
+  content  = "I Love Pets - ${random_pet.my_pet.id}"
 }
